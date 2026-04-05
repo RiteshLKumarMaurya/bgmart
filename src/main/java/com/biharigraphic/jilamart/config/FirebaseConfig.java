2e89e7a34e5cf9b88916a2f55e1525a5e0fc24b8
Ritesh Kumar <156053168+RiteshLKumarMaurya@users.noreply.github.com>
2026-04-05 21:05:44 +0530

    Update FirebaseConfig.java

--- src/main/java/com/biharigraphic/jilamart/config/FirebaseConfig.java
+++ src/main/java/com/biharigraphic/jilamart/config/FirebaseConfig.java
@@ -5,27 +5,47 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
+import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
 
     @PostConstruct
-    public void init() throws IOException {
+    public void init() {
+        try {
 
-        InputStream serviceAccount =
-                new ClassPathResource("firebase-service-account.json").getInputStream();
+            InputStream serviceAccount;
 
-        FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .build();
+            String path = "/etc/secrets/firebase-service-account.json";
+            File file = new File(path);
 
-        if (FirebaseApp.getApps().isEmpty()) {
-            FirebaseApp.initializeApp(options);
+            if (file.exists()) {
+                serviceAccount = new FileInputStream(file);
+                System.out.println("🔥 Firebase loaded from Render secrets");
+            } else {
+                serviceAccount = getClass().getClassLoader()
+                        .getResourceAsStream("firebase-service-account.json");
+
+                if (serviceAccount == null) {
+                    throw new RuntimeException("Firebase config not found");
+                }
+
+                System.out.println("🔥 Firebase loaded from local");
+            }
+
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 }
